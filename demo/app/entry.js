44a04d8c5e8682ec53fc9c9d4c5da0d12107de3e
devonhackley <devonhackley@gmail.com>
2017-05-23 12:16:15 -0700

    working through an angular example

--- demo/app/entry.js
+++ demo/app/entry.js
@@ -13,15 +13,35 @@ function CowsayController($log, $scope){
   $log.debug('#');
 
   let cowsayCtrl = $scope.cowsayCtrl = {};
+  cowsayCtrl.history = []
+  cowsayCtrl.currentText = ''
+  cowsayCtrl.sayText = '';
 
   cowsayCtrl.title = 'Welcome to cowsay'
 
   cowsayCtrl.speak = function(input){
     $log.debug('speak')
+    cowsayCtrl.sayText = input;
     return cowsay.say({text: input || 'blah', f:'dragon'})
   }
 
   cowsayCtrl.logger = function(input){
     $log.log(input)
+  }
+
+  cowsayCtrl.save = function(){
+    cowsayCtrl.history.push(cowsayCtrl.sayText);
+    cowsayCtrl.currentText = cowsayCtrl.history[cowsayCtrl.history.length - 1];
+    return cowsayCtrl.wat();
+  }
+
+  cowsayCtrl.undo = function(){
+    cowsayCtrl.history.pop();
+    cowsayCtrl.speak(cowsayCtrl.history[cowsayCtrl.history.length - 1]);
+    return cowsayCtrl.wat();
+  }
+
+  cowsayCtrl.wat = function(){
+    return cowsay.say({text: cowsayCtrl.currentText, f: 'cow'})
   }
 }
